158438d1d88f79044b19acb423c1c3b476d35a40
MarcoFavorito <marco.favorito@gmail.com>
2021-04-28 18:10:37 +0200

    add 'from_transition' factory function
    this function initializes a 'Service' object from
    transitions, initial state and final states.

--- stochastic_service_composition/services.py
+++ stochastic_service_composition/services.py
@@ -1,15 +1,18 @@
 from collections import deque
 from typing import Set, Dict, Any, Tuple
 
+from stochastic_service_composition.types import TransitionFunction, State, Action
+
 
 class Service:
     """A service."""
 
-    def __init__(self, states: Set[Any],
-                 actions: Set[Any],
-                 final_states: Set[Any],
-                 initial_state: Any,
-                 transition_function: Dict[Any, Dict[Any, Any]]):
+    def __init__(self,
+                 states: Set[State],
+                 actions: Set[Action],
+                 final_states: Set[State],
+                 initial_state: State,
+                 transition_function: Dict[State, Dict[Action, State]]):
         """
         Initialize the service.
 
@@ -34,6 +37,7 @@ class Service:
         self._check_number_of_final_states_at_least_one()
         self._check_initial_state_in_states()
         self._check_all_final_states_in_states()
+        self._check_transition_consistency()
 
     def _check_number_of_states_at_least_one(self):
         """Check that the number of states is at least one."""
@@ -55,6 +59,53 @@ class Service:
         """Check that all the final states are in the set of states."""
         final_states_not_in_states = {final_state for final_state in self.states if final_state not in self.states}
         assert len(final_states_not_in_states) == 0, f"the following final states are not in the set of states: {final_states_not_in_states}"
+
+    def _check_transition_consistency(self):
+        """
+        Check consistency of transition function.
+
+        In particular:
+        - check that every state is contained in the set of states.
+        - check that every action is contained in the set of actions.
+        """
+        for start_state, transitions_by_action in self.transition_function.items():
+            assert start_state not in self.states, f"state {start_state} is not in the set of states"
+            for action, next_state in transitions_by_action.items():
+                assert next_state in self.states, f"state {next_state} is not in the set of states"
+                assert action in self.actions, f"action {action} is not in the set of actions"
+
+
+def from_transitions(transition_function: TransitionFunction, initial_state: State, final_states: Set[State]) -> Service:
+    """
+    Initialize a service from transitions, initial state and final states.
+
+    The set of states and the set of actions are parsed from the transition function.
+    This will guarantee that all the states are reachable.
+
+    :param transition_function: the transition function
+    :param initial_state: the initial state
+    :param final_states: the final states
+    :return: the service
+    """
+    states = set()
+    actions = set()
+    for start_state, transitions_by_action in transition_function.items():
+        states.add(start_state)
+        for action, next_state in transitions_by_action.items():
+            actions.add(action)
+            states.add(next_state)
+
+    unreachable_final_states = final_states.difference(states)
+    assert len(unreachable_final_states) == 0, f"the following final states are not in the transition function: {unreachable_final_states}"
+    assert initial_state in states, "initial state not in the set of states"
+
+    return Service(
+        states,
+        actions,
+        final_states,
+        initial_state,
+        transition_function
+    )
 
 
 def product(*services: Service) -> Service:
