ee690a176207daca9aee6f5da2fcfb87102aacea
luusi <silo.1586010@studenti.uniroma1.it>
2021-05-26 20:30:29 +0200

    Adding config ThingsApi for digital twins

---
+++ digital_twins/Orchestrator.py
@@ -0,0 +1,30 @@
+import asyncio
+import websockets
+import base64
+import time
+import requests
+import json
+import subprocess
+import config
+from ThingsAPI import *
+#from composition import composition_mdp
+
+
+async def Orchestrator():
+    print("Opening websocket endpoint...")
+    uri = "wss://things.eu-1.bosch-iot-suite.com/ws/2"
+    async with websockets.connect(uri, extra_headers=websockets.http.Headers({'Authorization': 'Bearer ' + getToken()})) as websocket:
+        print("Collecting problem data...")
+       
+    #current_mdp_state= mdp.initial_state
+    #policy= mdp.get_policy_with_value_iteration()
+    #next_target_action= target.get_action()
+    #while true:
+        #current_mdp_state=(current_target_state, current_system_state, next_target_action)
+        #service_to_be_assigned=policy[current_mdp_state]
+
+        #next_target_action=target.get_action()
+
+result = asyncio.get_event_loop().run_until_complete(Orchestrator())
+while result == 1:
+    result = asyncio.get_event_loop().run_until_complete(Orchestrator())
---
+++ digital_twins/config.py
@@ -0,0 +1,6 @@
+ThingsAPI = dict(
+    namespace = 'com.bosch.services',
+    clientId = '88c89818-6c71-40e0-b7b0-6b77442c364f',
+    clientSecret = 'uJX-UAwA_jltMPzpUfKLk8uYro',
+    scope = 'service:iot-hub-prod:t6f04cf30b6b34842bfe43e6d9da37818_hub/full-access service:iot-things-eu-1:6f04cf30-b6b3-4842-bfe4-3e6d9da37818_things/full-access service:vorto'
+)
